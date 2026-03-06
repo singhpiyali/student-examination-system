a919d0240db7a70842a72bd7dbda73f9d048d384
Piyali Singh <106163266+singhpiyali@users.noreply.github.com>
2026-03-06 16:59:24 +0530

    Add files via upload

---
+++ admin/include/sidebar.php
@@ -0,0 +1,14 @@
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <ul class="nav side-menu">
+                  <li><a href="index.php"><i class="fa fa-tachometer"></i> Dashboard</a></li>
+                  <li><a href="course.php"><i class="fa fa-graduation-cap"></i> Courses</a></li>
+                  <li><a href="subject.php"><i class="fa fa-book"></i> Subject</a></li>
+                  <li><a href="question_bank.php"><i class="fa fa-pencil"></i> Question Bank</a></li>
+                  <li><a href="question_set.php"><i class="fa fa-file-text"></i> Exam Set</a></li>
+                  <li><a href="view_question.php"><i class="fa fa-eye"></i> View Questions</a></li>
+                  <li><a href="#"><i class="fa fa-users"></i> Students</a></li>
+                  <li><a href="#"><i class="fa fa-graduation-cap"></i> Result & Reoprts</a></li>
+                </ul>
+              </div>
+            </div>
